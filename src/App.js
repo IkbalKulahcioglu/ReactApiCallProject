54e898a9f6b56084eafda9a907870fd2df3df9d3
Ikbal Kulahcioglu <ikbalkulahcioglu@gmail.com>
2023-10-06 17:39:42 +0300

    Api.js added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import SearchHeader from './SearchHeader';
+import searchImages from './Api.js'
 
 function App() {
+ 
   const handleSubmit =(term) =>{
     console.log(term);
+    searchImages(term);
   };
   return (
     <div className="App">
